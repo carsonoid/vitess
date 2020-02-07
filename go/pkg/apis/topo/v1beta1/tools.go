1ab43007a93ae18ac3eb0132492184ccb5560a69
Carson Anderson <ca@carsonoid.net>
2020-02-07 15:15:09 -0700

    fixup

--- go/pkg/apis/topo/v1beta1/tools.go
+++
@@ -1,4 +0,0 @@
-package v1beta1
-
-// Import the code generator so that it gets installed with "go mod vendor"
-import _ "k8s.io/code-generator"
--- go/pkg/apis/topo/v1beta1/types.go
+++ go/pkg/apis/topo/v1beta1/types.go
@@ -5,54 +5,27 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// TestType is a top-level type. A client is created for it.
-type TestType struct {
+// VitessTopoNode is a container for Vitess topology data
+type VitessTopoNode struct {
 	metav1.TypeMeta `json:",inline"`
 	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	// +optional
-	Status TestTypeStatus `json:"status,omitempty"`
+	Data VitessTopoNodeData `json:"data"`
+}
+
+// VitessTopoNodeData contains the basic data for the node
+type VitessTopoNodeData struct {
+	Key   string `json:"key"`
+	Value string `json:"key"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// TestTypeList is a top-level list type. The client methods for lists are automatically created.
-// You are not supposed to create a separated client for this one.
-type TestTypeList struct {
+// VitessTopoNodeList is a top-level list type. The client methods for lists are automatically created.
+type VitessTopoNodeList struct {
 	metav1.TypeMeta `json:",inline"`
 	// +optional
 	metav1.ListMeta `json:"metadata,omitempty"`
 
-	Items []TestType `json:"items"`
-}
-
-type TestTypeStatus struct {
-	Blah string
-}
-
-// +genclient:nonNamespaced
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-type ClusterTestTypeList struct {
-	metav1.TypeMeta
-	metav1.ListMeta
-	Items []ClusterTestType
-}
-
-// +genclient
-// +genclient:nonNamespaced
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// +genclient:method=GetScale,verb=get,subresource=scale,result=k8s.io/kubernetes/pkg/apis/autoscaling.Scale
-// +genclient:method=UpdateScale,verb=update,subresource=scale,input=k8s.io/kubernetes/pkg/apis/autoscaling.Scale,result=k8s.io/kubernetes/pkg/apis/autoscaling.Scale
-
-type ClusterTestType struct {
-	metav1.TypeMeta `json:",inline"`
-	// +optional
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-	// +optional
-	Status ClusterTestTypeStatus `json:"status,omitempty"`
-}
-
-type ClusterTestTypeStatus struct {
-	Blah string
+	Items []VitessTopoNode `json:"items"`
 }
